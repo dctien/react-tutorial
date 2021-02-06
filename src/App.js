62e3582f0b2811605345e9c0b31f48cfb73edd84
DangCongTien <dctien00@gmail.com>
2021-02-06 21:23:49 +0700

    add components static

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Course from './Components/Course';
+// import { Gird } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="col-md-4 col-sm-4">
+           <Course />         
+           <Course />         
+           <Course />         
+      </div>
     </div>
   );
 }
---
+++ src/Components/Lession.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lession extends Component {
+    render() {
+        return (
+            <div>
+                <li className="list-group-item">Item 2</li>
+            </div>
+        )
+    }
+}
